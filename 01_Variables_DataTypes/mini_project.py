ed628bef91677ea7366f7d2ce096e8a1fa125014
Anuj <195161484+anuj7011@users.noreply.github.com>
2026-07-07 21:14:14 +0530

    Create mini_project.py

---
+++ 01_Variables_DataTypes/mini_project.py
@@ -0,0 +1,12 @@
+print("===== Student Information =====")
+
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+course = input("Enter Course: ")
+city = input("Enter City: ")
+
+print("\n----- Student Profile -----")
+print("Name   :", name)
+print("Age    :", age)
+print("Course :", course)
+print("City   :", city)
