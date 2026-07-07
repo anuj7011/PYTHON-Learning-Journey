563b6122d0ca656cf3375cc3be37b2d5b570e3fb
Anuj <195161484+anuj7011@users.noreply.github.com>
2026-07-07 21:12:15 +0530

    Create practice.py

---
+++ 01_Variables_DataTypes/practice.py
@@ -0,0 +1,104 @@
+# ==============================
+# Variables & Data Types Practice
+# ==============================
+
+# 1
+name = "Anuj"
+
+# 2
+age = 19
+
+# 3
+height = 5.8
+
+# 4
+is_student = True
+
+# 5
+print(name)
+
+# 6
+print(age)
+
+# 7
+print(type(name))
+
+# 8
+print(type(age))
+
+# 9
+print(type(height))
+
+# 10
+print(type(is_student))
+
+# 11
+x, y, z = 10, 20, 30
+print(x, y, z)
+
+# 12
+a = b = c = 100
+print(a, b, c)
+
+# 13
+city = "Delhi"
+
+# 14
+course = "Python"
+
+# 15
+salary = 25000.75
+
+# 16
+marks = 95
+
+# 17
+print(type(city))
+
+# 18
+print(type(course))
+
+# 19
+print(type(salary))
+
+# 20
+print(type(marks))
+
+# 21
+num = "100"
+print(int(num))
+
+# 22
+num = 50
+print(float(num))
+
+# 23
+price = 99.99
+print(int(price))
+
+# 24
+number = 10
+number = 20
+print(number)
+
+# 25
+PI = 3.14159
+print(PI)
+
+# 26
+name = input("Enter your name: ")
+print(name)
+
+# 27
+age = int(input("Enter your age: "))
+print(age)
+
+# 28
+print(type(age))
+
+# 29
+print(type(name))
+
+# 30
+country = "India"
+print(country)
