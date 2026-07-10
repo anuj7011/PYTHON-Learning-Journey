39ef8bcfce5f7808f325703d175efa86706c3516
Anuj <195161484+anuj7011@users.noreply.github.com>
2026-07-10 20:51:55 +0530

    Create mini_project.py

---
+++ 04_Tuples/mini_project.py
@@ -0,0 +1,17 @@
+print("===== Employee Record System =====")
+
+
+employee = (
+    101,
+    "Anuj Chauhan",
+    "Data Analytics",
+    45000
+)
+
+
+print("\nEmployee Details")
+
+print("Employee ID :", employee[0])
+print("Name        :", employee[1])
+print("Department  :", employee[2])
+print("Salary      :", employee[3])
